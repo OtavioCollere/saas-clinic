e5fb78a830311b224c3232a3c3810d298b02d0fb
OtavioCollere <otaviosk59@gmail.com>
2026-01-04 08:15:22 -0300

    feat : packages config and auth module

--- next-saas-rbac/packages/auth/src/index.ts
+++ next-saas-rbac/packages/auth/src/index.ts
@@ -1,3 +1,4 @@
+
 import {
   AbilityBuilder,
   CreateAbility,
@@ -5,9 +6,11 @@ import {
   ForcedSubject,
   MongoAbility,
 } from '@casl/ability'
+import type { User } from './models/user';
+import { permissions } from './permissions';
 
 const actions = ['manage', 'invite', 'delete'] as const
-const subjects = ['User', 'all'] as const
+const subjects = ['User', 'Project','all'] as const
 
 type AppAbilities = [
   (typeof actions)[number],
@@ -20,9 +23,16 @@ type AppAbilities = [
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
 
-const { can, cannot, build } = new AbilityBuilder(createAppAbility)
+export function defineAbilityFor(user : User) {
+  const builder = new AbilityBuilder(createAppAbility)
 
-can('invite', 'User')
-cannot('delete', 'User')
+  if(typeof permissions[user.role] !== 'function') {
+    throw new Error(`Permissions for role ${user.role} not found.`)
+  }
 
-export const ability = build()
+  permissions[user.role](user, builder)
+
+  const ability = builder.build()
+
+  return ability
+}
